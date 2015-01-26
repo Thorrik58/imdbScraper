e7adb8e493dedb0bd7467458022718c996b9c6f1
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2015-01-26 00:18:08 +0000

    Gets the title of the first item on the 250 imdb list

--- mysite/myBot/myBot/spiders/example.py
+++ mysite/myBot/myBot/spiders/example.py
@@ -4,12 +4,13 @@ from scrapy.spider import BaseSpider
 from myBot.items import MovieItem
 
 
-class ExampleSpider(BaseSpider):
+class example(BaseSpider):
     name = "example"
+    allowed_domains = ["imdb.com"]
     start_urls = (
         'http://www.imdb.com/chart/top?ref_=nv_ch_250/',
     )
 
     def parse(self, response):
-        title = response.xpath('//title')
-        return MovieItem(name=title)
+        for sel in response.xpath("//*[@class='chart']/tbody/tr/td[2]"):
+            return MovieItem(name=sel.xpath('a/text()').extract()[0])
